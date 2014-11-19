8ed417f3b8bdb662b7df43dd49ea7dc945646aaf
Austin McGee <austinwm@gmail.com>
2014-11-18 18:07:41 -0800

    Move all client source files to the src directory, run bower and copy static files in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var bower = require('bower');
 
 var watch = false;
 process.argv.forEach(function (val, index, array) {
@@ -10,22 +12,41 @@ process.argv.forEach(function (val, index, array) {
   }
 });
 
-gulp.task('default', function () {
+gulp.task('clean', function(){
+  return gulp.src(['www/*'], {read:false})
+    .pipe(clean());
+});
+
+gulp.task('bower', function(cb){
+  bower.commands.install([], {save: true}, {})
+    .on('end', function(installed){
+      cb(); // notify gulp that this task is finished
+    });
+});
+
+gulp.task('move', function(){
+  // the base option sets the relative root for the set of files,
+  // preserving the folder structure
+  gulp.src(['src/**/*.*', '!src/**/*.hbs'])
+  .pipe(gulp.dest('./www'));
+});
+
+gulp.task('hbs', function () {
     var templateData = {},
     options = {
-        batch : ['./src/partials'],
+        batch : ['./src/html/_partials'],
         helpers : {}
     }
 
-    return gulp.src('src/*.hbs')
+    return gulp.src(['src/html/**/*.hbs', '!src/html/_partials/**/*.*'])
         .pipe(handlebars(templateData, options))
         .pipe(rename(function (path) {
-		        path.extname = ".html"
-		    }))
+            path.extname = ".html"
+        }))
         .pipe(gulp.dest('./www'));
-});
+})
 
-if ( watch )
-{
-    var watcher = gulp.watch('src/**/*.hbs', ['default']);
-}
+gulp.task('default', ['bower','move', 'hbs']);
+gulp.task('watch', function() {
+  var watcher = gulp.watch('src/**/*.*', ['default']);
+})
