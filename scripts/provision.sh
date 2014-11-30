70a53be1421e33d82d63527438cefd2c34bd323b
Austin McGee <austinwm@gmail.com>
2014-11-29 20:25:05 -0800

    Serve static files through nginx rather than node/express.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,12 +6,10 @@ add-apt-repository ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
 
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-# echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-# sudo apt-get update
-# sudo apt-get install -y mongodb-org
-
-# sudo service mongod start
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
 if [ "$1" == "production" ]; then
 	git clone https://www.github.com/welldone/strato2.git /welldone
@@ -31,6 +29,7 @@ cd /welldone
 npm install
 
 ln -fs /welldone/scripts/upstart.conf /etc/init/strato.conf
+initctl reload-configuration
 service start strato
 
 ln -fs /welldone/scripts/nginx.cfg /etc/nginx/sites-enabled/default
