5bf4aaecae4f5422c00da43d49b5386263b2b768
Austin McGee <austinwm@gmail.com>
2015-02-01 14:46:26 -0800

    Migrate to mongodb.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+var BPromise = require('bluebird');
+
+
+var api = require('./lib/api/api.v1.0');
+var db = require('./lib/db');
 var server = require('./lib/server');
 
-var port = process.env['PORT'] || 3000;
-console.log("Starting server on port %d...", port);
-server.listen( port );
+console.log("Connecting to the database...");
+BPromise.join(
+	api.connect(),
+	db.connect()
+).then(function() {
+	var port = process.env['PORT'] || 3000;
+	console.log("Starting server on port %d...", port);
+	server.listen( port );
+})
