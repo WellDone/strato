d346790973eb2d9cf5fa2f71ba3e371db68b79e2
Austin McGee <austinwm@gmail.com>
2015-02-02 15:11:24 -0800

    Instantiate a new instance of the REM object.

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var BPromise = require('bluebird');
 
 var api = require('./lib/api/api.v1.0');
 var db = require('./lib/db');
-var server = require('./lib/server');
 
 console.log("Connecting to the database...");
 BPromise.join(
 	api.connect(),
 	db.connect()
 ).then(function() {
+	var server = require('./lib/server');
+	
 	var port = process.env['PORT'] || 3000;
 	console.log("Starting server on port %d...", port);
 	server.listen( port );
